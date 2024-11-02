52838f953e81bb08e7905ea8b82c769fec940163
Haseeb Yaseen <haseebyaseen662@gmail.com>
2024-11-02 14:11:56 +0500

    update

--- resources/views/authentications/login.blade.php
+++ resources/views/authentications/login.blade.php
@@ -11,17 +11,37 @@
     <meta name="Author" content="TECHSOPS Private Limited">
     <meta name="keywords" content="">
 
-    <!-- Favicon -->
-    <link rel="icon" href="{{ asset('admin/assets/images/brand-logos/favicon.ico') }}" type="image/x-icon">
+    <!-- Favicon icon -->
+    <link rel="icon" href="{{ asset('admin/assets/images/favicon.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('admin/assets/images/favicon.png') }}" type="image/x-icon">
 
-    <!-- Bootstrap Css -->
-    <link id="style" href="{{ asset('admin/assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900;6..12,1000&display=swap"
+        rel="stylesheet">
 
-    <!-- Style Css -->
-    <link href="{{ asset('admin/assets/css/styles.min.css') }}" rel="stylesheet">
+    <!-- Flag icon css -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/vendors/flag-icon.css') }}">
 
-    <!-- Font Awesome (for social media icons) -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- iconly-icon -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/iconly-icon.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/bulk-style.css') }}">
+
+    <!-- themify-icon -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/themify.css') }}">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/fontawesome-min.css') }}">
+
+    <!-- Weather Icon css -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/vendors/weather-icons/weather-icons.min.css') }}">
+
+    <!-- App css -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+    <link id="color" rel="stylesheet" href="{{ asset('admin/assets/css/color-1.css') }}" media="screen">
+
 
     <style>
         /* Ensure the logo fits well on mobile screens */
@@ -39,91 +59,110 @@
 
         .error-message {
             color: red;
-            font-size: 0.875em; /* Slightly smaller font size for errors */
+            font-size: 0.875em;
+            /* Slightly smaller font size for errors */
         }
 
         @media (max-width: 767.98px) {
             .logo-container {
-                padding: 10px; /* Reduce padding on smaller screens */
+                padding: 10px;
+                /* Reduce padding on smaller screens */
             }
 
             .login {
-                padding: 20px; /* Add some padding for better spacing */
+                padding: 20px;
+                /* Add some padding for better spacing */
             }
         }
     </style>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <!-- The logo half -->
-            <div class="col-md-6 d-none d-md-flex justify-content-center align-items-center bg-white">
-                <div class="logo-container">
-                    <img src="{{ asset('admin/assets/images/brand-logos/logo.png') }}" alt="Logo" class="img-fluid">
-                </div>
-            </div>
-            <!-- The form half -->
-            <div class="col-md-6 bg-white py-4">
-                <div class="login d-flex align-items-center py-2">
-                    <div class="container p-0">
-                        <div class="row">
-                            <div class="col-md-10 mx-auto">
-                                <div class="card-sigin">
-                                    <div class="main-signup-header">
-                                        <h3>Welcome back!</h3>
-                                        <h6 class="fw-medium mb-4 fs-17">Please sign in to continue.</h6>
-                                        <form method="POST" action="{{ route('login') }}">
-                                            @csrf
-
-                                            <div class="form-group mb-3">
-                                                <label class="form-label">Email</label>
-                                                <input class="form-control" name="email" placeholder="Enter your email" type="email" required>
-                                                @error('email')
-                                                    <div class="error-message">{{ $message }}</div>
-                                                @enderror
-                                            </div>
-
-                                            <div class="form-group mb-3">
-                                                <label class="form-label">Password</label>
-                                                <input class="form-control" name="password" placeholder="Enter your password" type="password" required>
-                                                @error('password')
-                                                    <div class="error-message">{{ $message }}</div>
-                                                @enderror
-                                            </div>
-
-                                            <button type="submit" class="btn btn-primary btn-block w-100">Sign In</button>
-
-                                            <div class="row mt-3">
-                                                <div class="col-sm-6">
-                                                    <button class="btn btn-block w-100 btn-facebook">
-                                                        <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                                                    </button>
-                                                </div>
-                                                <div class="col-sm-6 mt-2 mt-sm-0">
-                                                    <button class="btn btn-block w-100 btn-google" style="background-color: #db4437; color: white;">
-                                                        <i class="fab fa-google me-2"></i> Sign in with Google
-                                                    </button>
-                                                </div>
-                                            </div>
-
-                                            <div class="main-signin-footer mt-4">
-                                                <p class="mb-1"><a href="{{ route('password.request')}}">Forgot password?</a></p>
-                                                <p>Don't have an account? <a href="{{ route('register.show') }}">Create an Account</a></p>
-                                            </div>
-                                        </form>
+    <!-- tap on top starts-->
+    <div class="tap-top"><i class="iconly-Arrow-Up icli"></i></div>
+    <!-- tap on tap ends-->
+    <!-- loader-->
+    <div class="loader-wrapper">
+        <div class="loader"><span></span><span></span><span></span><span></span><span></span></div>
+    </div>
+    <!-- login page start-->
+    <div class="container-fluid p-0">
+        <div class="row m-0">
+            <div class="col-12 p-0">
+                <div class="login-card login-dark">
+                    <div>
+                        <div><a class="logo" href="index.html"><img class="img-fluid for-light m-auto"
+                                    src="{{ asset('admin/assets/images/logo/logo1.png') }}" alt="looginpage"><img
+                                    class="img-fluid for-dark"
+                                    src="{{ asset('admin/assets/images/logo/logo-dark.png') }}" alt="logo"></a>
+                        </div>
+                        <div class="login-main">
+                            <form class="theme-form">
+                                <h2 class="text-center">Sign in to account</h2>
+                                <p class="text-center">Enter your email &amp; password to login</p>
+                                <div class="form-group">
+                                    <label class="col-form-label">Email Address</label>
+                                    <input class="form-control" type="email" required=""
+                                        placeholder="user@gmail.com">
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-form-label">Password</label>
+                                    <div class="form-input position-relative">
+                                        <input class="form-control" type="password" name="login[password]"
+                                            required="" placeholder="*********">
+                                        <div class="show-hide"><span class="show"> </span></div>
                                     </div>
                                 </div>
-                            </div>
+                                <div class="form-group mb-0 checkbox-checked">
+                                    <div class="form-check checkbox-solid-info">
+                                        <input class="form-check-input" id="solid6" type="checkbox">
+                                        <label class="form-check-label" for="solid6">Remember password</label>
+                                    </div><a class="link" href="forget-password.html">Forgot password?</a>
+                                    <div class="text-end mt-3">
+                                        <button class="btn btn-primary btn-block w-100" type="submit">Sign in </button>
+                                    </div>
+                                </div>
+                                <div class="login-social-title">
+                                    <h6>Or Sign in with </h6>
+                                </div>
+                                <div class="form-group">
+                                    <ul class="login-social">
+                                        <li><a href="https://www.linkedin.com" target="_blank"><i
+                                                    class="icon-linkedin"></i></a></li>
+                                        <li><a href="https://twitter.com" target="_blank"><i
+                                                    class="icon-twitter"></i></a></li>
+                                        <li><a href="https://www.facebook.com" target="_blank"><i
+                                                    class="icon-facebook"></i></a></li>
+                                        <li><a href="https://www.instagram.com" target="_blank"><i
+                                                    class="icon-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                                <p class="mt-4 mb-0 text-center">Don't have account?<a class="ms-2"
+                                        href="sign-up.html">Create
+                                        Account</a></p>
+                            </form>
                         </div>
-                    </div><!-- End -->
+                    </div>
                 </div>
-            </div><!-- End -->
+            </div>
         </div>
-    </div>
+        <!-- jQuery -->
+        <script src="{{ asset('admin/assets/js/vendors/jquery/jquery.min.js') }}"></script>
 
-    <!-- Bootstrap JS -->
-    <script src="{{ asset('admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <!-- Bootstrap JS -->
+        <script src="{{ asset('admin/assets/js/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}" defer></script>
+        <script src="{{ asset('admin/assets/js/vendors/bootstrap/dist/js/popper.min.js') }}" defer></script>
+
+        <!-- Fontawesome -->
+        <script src="{{ asset('admin/assets/js/vendors/font-awesome/fontawesome-min.js') }}"></script>
+
+        <!-- Password Show -->
+        <script src="{{ asset('admin/assets/js/password.js') }}"></script>
+
+        <!-- Custom Script -->
+        <script src="{{ asset('admin/assets/js/script.js') }}"></script>
+
+    </div>
 </body>
 
 </html>

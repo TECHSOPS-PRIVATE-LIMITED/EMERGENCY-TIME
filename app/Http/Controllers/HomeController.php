2dc09ff933cfffb39895d114fc88aa00d0f25257
muneeb9000 <championsays786@gmail.com>
2024-11-14 19:23:19 +0500

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use App\Models\Timezone;
 use App\Models\User;
 use App\Models\Provider;
 use Illuminate\Support\Facades\Hash;
-
+use Spatie\Permission\Models\Role;
 class HomeController extends Controller
 {
     public function index()
